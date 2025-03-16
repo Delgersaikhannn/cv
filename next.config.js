760501f22117eb2bc0637915fabc422f5cea2e6a
Delgersaikhannn <Delgersaikhannn@users.noreply.github.com>
2025-03-16 17:57:59 +0000

    deploy: 8089c6cd186ebf4379ebb72df31e28fe4626cf74

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    unoptimized: true, // Disable image optimization for static export
-  },
-};
-
-module.exports = nextConfig;
--- src/utils/globals.js
+++
@@ -1,1 +0,0 @@
-export const Container = ["100%", "100%", "700px", "700px", "800px", "1000px"];
